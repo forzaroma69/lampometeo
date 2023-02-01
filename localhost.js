f98b8569abf96e3ceb2add843c39248973323af1
forzaroma69 <japoko2004@gmail.com>
2023-02-01 14:16:40 +0100

    ho fatto rispondere dal server q

--- localhost.js
+++ localhost.js
@@ -1,10 +1,6 @@
 var http = require('http');
-var fs = require('fs');
 
 http.createServer(function (req, res) {
-  fs.readFile('Lampometeo.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
 }).listen(8080);
